33faf29847709409b9a14d89fd141a625c0e141b
Brotrueck, Michael <mbrotrueck@avp.de>
2020-02-05 16:59:09 +0100

    change path for configuration storage from %APPDATA%\Remotely to %APPDATA%\AvP QuickSupport

--- Desktop.Win/Services/Config.cs
+++ Desktop.Win/Services/Config.cs
@@ -12,8 +12,8 @@ namespace Remotely.Desktop.Win.Services
     public class Config
     {
 public string Host { get; set; } = "https://quicksupport.avp.de";
-        private static string ConfigFile => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Remotely", "Config.json");
-        private static string ConfigFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Remotely");
+        private static string ConfigFile => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AvP Quicksupport", "Config.json");
+        private static string ConfigFolder => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AvP Quicksupport");
         public static Config GetConfig()
         {
             if (!Directory.Exists(ConfigFolder))
